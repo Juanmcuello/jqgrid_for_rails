88482156f9cdb9f832fffd0d9403bf441980bcfd
Juan Manuel Cuello <juanmacuello@gmail.com>
2011-06-08 16:44:58 -0300

    View helpers are now tested using ActionView::TestCase.

--- test/jqgrids_helper_test.rb
+++ test/jqgrids_helper_test.rb
@@ -1,14 +1,17 @@
+require "action_view/test_case"
 require File.dirname(__FILE__) + '/test_helper.rb'
 
 class MockView < ActionView::Base
   include JqgridsHelper
 end
 
-class JqgridsHelperTest < Test::Unit::TestCase
+class JqgridsHelperTest < ActionView::TestCase
 
-  def test_jqgrid_small
-
+  def setup
     @template = MockView.new
+  end
+
+  test "jqgrid small" do
 
     grid_id = 'grid_id'
     grid_options = [{:url => "/jqGridModel?model=Wine" }]
@@ -18,12 +21,9 @@ class JqgridsHelperTest < Test::Unit::TestCase
     expected << '</script>'
 
     assert_equal(expected, @template.jqgrid_api(grid_id, grid_options, {:script_tags => true}))
-
   end
 
-  def test_jqgrid_medium
-
-    @template = MockView.new
+  test "jqgrid_medium" do
 
     grid_id       = 'grid_id'
     pager_id      = 'gridpager'
@@ -38,23 +38,7 @@ class JqgridsHelperTest < Test::Unit::TestCase
     assert_equal(expected, @template.jqgrid_api(grid_id, grid_options, options))
   end
 
-  def expected_grid grid_id, pager_id, options
-
-    options[:script_tags] ||= true
-
-    js =  'jQuery(document).ready(function() {
-            jQuery("#' + grid_id + '").jqGrid({
-              "pager":jQuery(\'#' + pager_id + '\')
-            });
-          });'
-    js.gsub!(/\n\s+/, '')
-    js = "<script>\n#{js}\n</script>" if options[:script_tags]
-    js
-
-  end
-
-  def test_jqgrid_nav_options
-    @template = MockView.new
+  test "jqgrid nav_options" do
 
     grid_id       = 'grid_id'
     pager_id      = 'pager_id'
@@ -69,17 +53,14 @@ class JqgridsHelperTest < Test::Unit::TestCase
     assert_equal(expected, @template.jqgrid_api(grid_id, [grid_options], [:navGrid, "##{pager_id}", {:del => true }, {}, {}, {:closeOnEscape => true}], options))
   end
 
-  def test_pager_id_from_options
-    @template = MockView.new
-
+  test "pager_id_from_options" do
     assert_equal 'my_pager_div', @template.send(:pager_id_from_options, {:pager => "jQuery('#my_pager_div')"})
     assert_equal 'my_pager_div', @template.send(:pager_id_from_options, {:pager => 'jQuery("#my_pager_div")'})
     assert_equal 'my_pager_div', @template.send(:pager_id_from_options, {:pager => "#my_pager_div"})
     assert_equal 'my_pager_div', @template.send(:pager_id_from_options, {:pager => "my_pager_div"})
   end
 
-  def test_chained_functions
-    @template = MockView.new
+  test "chained_functions" do
     div_id = 'my_grid'
     nav_button =  [:navButtonAdd, '#pager', { :caption => 'Add'}]
     nav_separator =  [:navSeparatorAdd, '#pager']
